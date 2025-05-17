5b9f107349d3c1d81477c54f4501d3d626d46eff
pablofalanga <pablofalanga45@gmail.com>
2025-05-17 15:03:13 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  // "./offline.html" // Descomenta si agregás una página offline
+];
+
+// Evento de instalación
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Evento de activación
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// de fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        // return caches.match("./offline.html"); // Descomenta si agregás una página offline
+      });
+    })
+  );
+});
